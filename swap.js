44b5fc43a40de811a3b2ab18d2b83e33d29e1897
Anusha N <123183455+Anushamurthyn@users.noreply.github.com>
2024-02-07 22:12:34 +0530

    adding new files

---
+++ swap.js
@@ -0,0 +1,51 @@
+// var num1= 10;
+// var num2=20;
+
+// console.log("Before swapping", num1, num2)
+// function swap(){
+   
+//     num1 = num1 + num2;
+//     num2 = num1 - num2;
+//     num1 = num1 - num2;
+//     console.log("after swapping", num1, num2)
+    
+// }
+// swap()
+
+//palindrome
+// let num = 121
+// let originalNum= num
+// var rev =0;
+// while(num!==0){
+//     rem = num%10;
+//     rev = rev*10 + rem;
+//     num = Math.floor(num/10)
+
+// }
+// if(originalNum === rev){
+//     console.log("palindrome", rev)
+// }else{
+// console.log("not palindrome", rev)}
+
+
+//palindrome of a string
+// let originalString = "MOM"
+// let palString = " "
+// l= originalString.length;
+// let isPalindrome = true;
+// for(let i=0; i<l ; i++){
+//     for(let j=i+1; j<i; j++){
+//         if(originalString[i]!==originalString[j]){
+//             isPalindrome = false;
+//             break;
+//         }
+        
+//     }
+// }
+// if (isPalindrome) {
+//     console.log(`${originalString} is a palindrome`);
+// } else {
+//     console.log(`${originalString} is not a palindrome`);
+// }
+
+//
